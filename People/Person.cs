9544c56b88df647a06c320cf7e2acd362426e242
Glaston D'souza <glastondsouza97@gmail.com>
2020-08-17 11:16:36 +0530

    Initial Commit

---
+++ People/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstProject.People
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greetings()
+        {
+            Console.WriteLine("Hii, my name is " + Name + " and my age is " + Age);
+        }
+
+        public static int Calc(int x, int y)
+        {
+            int number = x + y;
+            return number;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using FirstProject.People;
+using System;
+
+namespace FirstProject
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Hello", "HI" };
+            Console.WriteLine(names[0]);
+        }
+    }
+}
